a37a9670679f2e38cd873fc5e704c9207b36080b
rpitman <ronnie.pitman@gmail.com>
2018-04-03 16:07:51 -0400

    Initial Commit

---
+++ Template SpriteKit Rebeloper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Template SpriteKit Rebeloper
+//
+//  Created by ronnie on 4/3/18.
+//  Copyright © 2018 space.c-flat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
